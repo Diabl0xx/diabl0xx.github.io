caa8a06dec7f6af1595980cea58ee64b4ae160ab
Diabl0xxf5 <119334965+Diabl0xxf5@users.noreply.github.com>
2024-04-17 09:46:50 +0500

    rew
    rew

--- web_app.js
+++ web_app.js
@@ -1,6 +1,7 @@
 let error_text = document.getElementById("error_msg");
 let tg = window.Telegram.WebApp;
 tg.expand();
+
 error_text.innerText = tg.initData;
 let b_create_task = document.getElementById("create_task");
 
@@ -20,23 +21,14 @@ function mDebug() {
 
 b_create_task.addEventListener("click", function() {
 
-    //let usr_id = tg.initDataUnsafe.user.id;
-    let usr_id = "123";
-    let usr_name = document.getElementById("user_name").value;
-    let usr_phone = document.getElementById("user_phone").value;
     let tsk_topic = document.getElementById("task_topic").value;
     let tsk_text = document.getElementById("task_text").value;
 
-    if (!checkField(usr_name, "Имя")
-        || !checkField(usr_phone, "Телефон для связи")
-        || !checkField(tsk_topic, "Тема задачи")
+    if (!checkField(tsk_topic, "Тема задачи")
         || !checkField(tsk_text, "Постановка задачи"))
             return;
 
     let data = {
-        usr_id: usr_id,
-        usr_name: usr_name,
-        usr_phone: usr_phone,
         tsk_topic: tsk_topic,
         tsk_text: tsk_text
     }
